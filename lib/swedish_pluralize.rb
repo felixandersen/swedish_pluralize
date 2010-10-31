0af0c9733a86443c9b31f26f18aa96d36d3fac16
Felix Andersen <felix@istocat.se>
2010-10-31 20:49:08 +0100

    Rules added
    Pluralization fails 5345 out of 9922 words

--- lib/swedish_pluralize.rb
+++ lib/swedish_pluralize.rb
@@ -2,15 +2,21 @@ module SwedishPluralize
   def self.inflections_plural(i)
     # declination 1 -or
     i.plural /a$/i, 'or' # flicka, flickor
+    i.plural /s$/i, 'sor' #ros, rosor
 
     # declination 2 -ar
-    i.plural /e$/i, 'ar' # pojke, pojkar
+    i.plural /([^r][^r]|rr)e$/i, '\1ar' # pojke, pojkar, (not spridare, spridare but aborre, abborrar)
     i.plural /g$/i, 'gar'
     i.plural /el$/i, 'ar' # fågel, fåglar
     i.plural /l$/i, 'lar' # bil, bilar
+    i.plural /app$/i, 'appar' #lapp, lappar
 
     # declination 3 -er
-    i.plural /([tmnslpr])$/i, '\1er' # produkt, produkter
+    i.plural /([tmnlr])$/i, '\1er' # produkt, produkter
+    i.plural /upp$/i, 'upper'
+    i.plural /([^o])s$/i, '\1ser'
+    i.plural /([^p])p$/i, '\1per'
+    
     i.plural /kel$/i, 'kler' # muskel, muskler
     i.plural /tel$/i, 'tlar' # titel, titlar
 
@@ -19,20 +25,36 @@ module SwedishPluralize
 
     # declination 5 -n
     i.plural /le$/i, 'lem' # muskel, muskler
+    i.plural /de$/i, 'den' # åliggande, åliggander
+    
+    i.plural /([^bd])ag$/i, '\1ag' #landslag, landslag
+    i.plural /([de])lag$/i, '\1lagar' #grundlag, grundlagar
+    
+    i.plural /iende$/i, 'iender' # fiende, fiender
+    i.plural /(i|än)de$/i, '\1der' # guide, guider, frände, fränder
 
     i.plural /ö$/i, 'ön' #frö, frön
     i.plural /ok$/i, 'öcker' #bok, böcker
     i.plural /um$/i, 'a' #faktum, fakta
     i.plural /o$/i, 'or' #sko, skor
     i.plural /and$/i, 'änder' #hand, händer
-    i.plural /s$/i, 'sor' #ros, rosor
+
     i.plural /man$/i, 'män' #man, män
     i.plural /mus$/i, 'möss' #mus, möss
-    i.plural /d$/i, 'den' #huvud, huvuden
+    i.plural /([^n])d$/i, '\1den' #huvud, huvuden, but should let lund and such through 
     i.plural /ne$/i, 'nen' #vittne, vittnen
 
     # Own hack
+    i.plural /([^l])ie?$/i, '\1ier' #kategori, kategorier - aktie, aktier
     i.plural /i$/i, 'ier' #kategori, kategorier
+    
+    # i.plural /(^.+)del$/i, '\1dlar' #sedel, sedlar and bödel, bödlar but NOT del, delar
+    # i.plural /^å$/i, 'åar' #å, åar
+    # i.plural /are$/i, 'are' #åklagare, härförare och A-lagare
+    # i.plural /é$/i, 'éer' #idé, idéer
+    # i.plural /nting/i, 'nting' #mellanting
+    # i.plural /([ngj])em([åäa])ng$/i, '\1em\2ng' #abonemang, arrangemang, handgemäng
+    #i.plural /ns$/i, 'nser' #konduktans, konduktanser
   end
 
   def self.inflections_singular(i)
@@ -67,10 +89,14 @@ module SwedishPluralize
     i.singular /änder$/i, 'and' #hand, händer
     i.singular /sor$/i, 's' #ros, rosor
 
+    #untouchable endings
+    i.singular /dagis$/i, 'dagis'
+    i.singular /bevis$/i, 'bevis'
+    i.singular /paradis$/i, 'paradis'
   end
 
   def self.inflections_uncountable(i)
-    i.uncountable %w(hus kar träd får brev namn nummer kön)
+    i.uncountable %w(hus kar träd får brev namn nummer kön dualis fritis pris kondis lekis pluralis ris singularis)
   end
 
   def self.inflections_irregular(i)
